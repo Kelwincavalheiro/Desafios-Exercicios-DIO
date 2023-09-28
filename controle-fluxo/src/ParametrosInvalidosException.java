a6c962fe32636c6cbdac76678eb2f6daf4bc87aa
Kelwin Cavalheiro <kelwinkiwi@gmail.com>
2023-09-28 17:30:27 -0300

    adicionando controle-fluxo

---
+++ controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+    public class ParametrosInvalidosException extends Exception {
+        // Construtor padrão
+        public ParametrosInvalidosException() {
+            super("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
